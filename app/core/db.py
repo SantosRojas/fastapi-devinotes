2992d7dea92247c972f893feafb85bfc1c4b0429
Ricardo Cuéllar <rcuellarsn@gmail.com>
2025-10-27 21:46:44 -0600

    Add database file configuration

---
+++ app/core/db.py
@@ -0,0 +1,18 @@
+
+from typing import Iterator
+from sqlmodel import SQLModel, Session, create_engine
+
+from app.core.config import settings
+
+
+engine = create_engine(settings.DATABASE_URL, echo=False, connect_args={
+                       "check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {})
+
+
+def init_db() -> None:
+    SQLModel.metadata.create_all(engine)  # dev
+
+
+def get_session() -> Iterator[Session]:
+    with Session(engine) as session:
+        yield session
