51a5c5460f8242aa59966b4be5176a6c8b2e0ab7
SantosRojas <santos777rojas@gmail.com>
2025-12-25 13:17:41 -0500

    pp

--- app/core/db.py
+++ app/core/db.py
@@ -5,21 +5,21 @@ from sqlmodel import SQLModel, Session, create_engine
 
 from app.core.config import settings
 
-# raw_url = os.environ["DATABASE_URL"]
-# url = raw_url
-# if url.startswith("postgres://"):
-#     url = "postgresql+psycopg://" + url[len("postgres://"):]
-# elif url.startswith("postegresql://") and "psycopg" not in url:
-#     url = "postgresql+psycopg://" + url[len("postgresql://"):]
+raw_url = os.environ["DATABASE_URL"]
+url = raw_url
+if url.startswith("postgres://"):
+    url = "postgresql+psycopg://" + url[len("postgres://"):]
+elif url.startswith("postegresql://") and "psycopg" not in url:
+    url = "postgresql+psycopg://" + url[len("postgresql://"):]
 
-engine = create_engine(settings.DATABASE_URL, echo=False, connect_args={
-                       "check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {}) #for dev
+# engine = create_engine(settings.DATABASE_URL, echo=False, connect_args={
+#                        "check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {}) #for dev
 
-# engine = create_engine(
-#     url,
-#     echo=False,
-#     pool_pre_ping=True,
-# )
+engine = create_engine(
+    url,
+    echo=False,
+    pool_pre_ping=True,
+)
 
 def init_db() -> None:
     pass
